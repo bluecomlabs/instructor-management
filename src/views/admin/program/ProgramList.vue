a2cbf3cf41d501eacb681fab22e1a57e3de7127d
jaebum26 <dlwoqja013@naver.com>
2024-11-29 10:41:32 +0900

    chore: 프로그램 조회 미디어 쿼리 표 수정

--- src/views/admin/program/ProgramList.vue
+++ src/views/admin/program/ProgramList.vue
@@ -84,7 +84,7 @@
       </div>
     </div>
 
-    <div class="card-body pt-0">
+    <div class="card-body pt-0 com-headerCon">
       <div v-if="isLoading" class="overlay">
         <div class="loader"></div>
       </div>
@@ -202,6 +202,89 @@
         </nav>
       </div>
     </div>
+    <div class="card-body pt-0 mob-headerCon">
+      <div v-if="isLoading" class="overlay">
+        <div class="loader"></div>
+      </div>
+
+      <KTDatatable
+        @on-sort="sort"
+        @on-items-select="onItemSelect"
+        :data="data"
+        :header="mobheaderConfig"
+        @selection-change="onSelectionChange"
+      >
+        
+        <template v-slot:aboutProgram="{ row: customer }">
+          <span :class="`badge py-3 px-4 fs-7 badge-light-${statusColor[customer.status]}`">
+            <div class="column-aboutProgram" @click="onProgramClick(customer)" style="cursor: pointer;">
+              {{ customer.chapter }} - {{ customer.programName }}
+            </div>
+          </span>
+        </template>
+        <template v-slot:product="{ row: customer }">
+          <div class="column-product">{{ customer.product }}</div>
+        </template>
+        <template v-slot:level="{ row: customer }">
+          <div class="column-level">{{ customer.level }}</div>
+        </template>
+      </KTDatatable>
+
+      <div class="d-flex justify-content-end mt-4">
+        <nav aria-label="Page navigation">
+          <ul class="pagination">
+            <li
+              class="page-item"
+              :class="{ disabled: currentPage === 0 }"
+              @click="onPageChange(0)"
+            >
+              <a class="page-link">
+                <i class="ki-duotone ki-double-left fs-2">
+                  <span class="path1"></span>
+                  <span class="path2"></span>
+                </i>
+              </a>
+            </li>
+            <li
+              class="page-item"
+              :class="{ disabled: currentPage === 0 }"
+              @click="onPageChange(currentPage - 1)"
+            >
+              <i class="page-link ki-duotone ki-left fs-2"></i>
+            </li>
+            <li
+              class="page-item"
+              v-for="page in visiblePages"
+              :key="page"
+              :class="{ active: page === currentPage + 1 }"
+              @click="onPageChange(page - 1)"
+            >
+              <a class="page-link" href="#">{{ page }}</a>
+            </li>
+            <li
+              class="page-item"
+              :class="{ disabled: currentPage + 1 === totalPages }"
+              @click="onPageChange(currentPage + 1)"
+            >
+              <i class="page-link ki-duotone ki-right fs-2"></i>
+            </li>
+            <li
+              class="page-item"
+              :class="{ disabled: currentPage + 1 === totalPages }"
+              @click="onPageChange(totalPages - 1)"
+            >
+              <a class="page-link">
+                <i class="ki-duotone ki-double-right fs-2">
+                  <span class="path1"></span>
+                  <span class="path2"></span>
+                </i>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -369,6 +452,27 @@ export default defineComponent({
         columnLabel: "createdAt",
         sortEnabled: false,
         columnWidth: 150,
+      },
+    ]);
+
+    const mobheaderConfig = ref([
+      {
+        columnName: "총 차시 - 프로그램명",
+        columnLabel: "aboutProgram",
+        sortEnabled: true,
+        columnWidth: 100,
+      },
+      {
+        columnName: "교구",
+        columnLabel: "product",
+        sortEnabled: true,
+        columnWidth: 50,
+      },
+      {
+        columnName: "레벨",
+        columnLabel: "level",
+        sortEnabled: true,
+        columnWidth: 50,
       },
     ]);
 
@@ -645,6 +749,7 @@ export default defineComponent({
       searchItems,
       data,
       headerConfig,
+      mobheaderConfig,
       currentPage,
       totalPages,
       onPageChange,
@@ -700,6 +805,15 @@ export default defineComponent({
   0% { transform: rotate(0deg); }
   100% { transform: rotate(360deg); }
 }
+
+.com-headerCon {
+  display: inline;
+}
+
+.mob-headerCon {
+  display: none;
+}
+
 .column-status,
 .column-programName,
 .column-chapter,
@@ -800,6 +914,14 @@ export default defineComponent({
   .mobile-text {
     display: inline;
   }
+  
+  .com-headerCon {
+    display: none;
+  }
+
+  .mob-headerCon {
+    display: inline;
+  }
 
   .del-selected {
     display: none;
@@ -829,24 +951,23 @@ export default defineComponent({
     display: block;
     margin-bottom: 15px;
   }
-  .column-isConfirmed,
-  .column-institutionName,
-  .column-programName,
-  .column-createdAt,
-  .column-grade,
-  .column-classNumber,
-  .column-numberOfStudents,
-  .column-date,
-  .column-remark {
+  .column-aboutProgram {
+    width: 100px; 
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: inline-block;
+  }
+  .column-product,
+  .column-level {
     display: block;
-    width: auto; /* 너비를 자동으로 조정 */
-    white-space: normal; /* 텍스트가 너무 길면 자동 줄 바꿈 */
+    width: auto;
+    white-space: normal;
     overflow: visible;
-    text-overflow: clip; /* 넘치는 텍스트를 표시하지 않음 */
+    text-overflow: clip;
     margin: 10px 0;
   }
 
-  /* 열을 세로로 나열하면서 셀 내용에도 스타일 적용 */
   .table-row > div {
     display: block;
     padding: 10px;
